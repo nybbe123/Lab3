35c144f9580a21754c4add1138e0ee619bc6af41
Jonatan Nyberg <33462941+nybbe123@users.noreply.github.com>
2022-05-02 14:25:02 +0200

    setting up Socket.io logic

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:3001');
 
 function App() {
+  const [username, setUsername] = useState('');
+
+  const joinRoom = () => {
+
+  }
+
   return (
     <div>
       <h1>Hello world</h1>
+      <h4>Enter Name:</h4>
+      <input type="text" placeholder='John Wick...' onChange={(event) => {setUsername(event.target.value)}} />
+      <button>JOIN</button>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const app = express();
+const http = require('http'); //Req to build server with socket.io
+const cors = require('cors'); //Req for secure cross-origin requests and data transfers between browsers and servers.
+const { Server } = require('socket.io');
 
-var port = process.env.PORT || 3000;
+app.use(cors());
 
-// ROUTES
-app.get('/', (req, res) => {
-    res.send('Red Light Chat. ft. Emil & Jonte & Maxiboi & Simme')
-})
+const server = http.createServer(app);
 
-app.listen(port, () => {
+var port = process.env.PORT || 3001;
+
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'DELETE']
+    },
+});
+
+io.on('connection', (socket) => {
+    console.log(`User Connected with ID: ${socket.id}`);
+
+    io.on('disconnect', (socket) => {
+        console.log('User disconnected', socket.id);
+    })
+});
+
+server.listen(port, () => {
     console.log(`Server is working on port ${port}`)
-})
+});
