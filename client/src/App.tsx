40ab4e6ee863f5ed271fe383b981137d825cd833
Max Andersson <maxbjornkga@gmail.com>
2022-05-03 14:33:39 +0200

    empty

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,41 +13,41 @@ const initalMessagesState = {
 function App() {
   const [username, setUsername] = useState("");
   const [connected, setConnected] = useState(false);
-  const [currentChat, setCurrentChat] = useState({ isChannel: true, chatId: "general", recieverId: ""});
+  const [currentChat, setCurrentChat] = useState({ isChannel: true, chatId: "general", recieverId: "" });
   const [connectedRooms, setConnectedRooms] = useState(["general"]);
   const [allUsers, setAllUsers] = useState([]);
   const [messages, setMessages] = useState(initalMessagesState);
   const [message, setMessage] = useState("");
 
   let body;
-  if(connected) {
-    body = {
-      <Chat
-      message={message}
-      handleMe
-    }
-  }
+  // if(connected) {
+  //   body = {
+  //     <Chat
+  //     message={message}
+  //     handleMe
+  //   }
+  // }
 
   return (
     <div>
       <h1>Chathouse</h1>
       <h4>Enter Name:</h4>
-      <input 
-        type="text" 
+      <input
+        type="text"
         placeholder='Enter your nickname'
-        label="Nickname" 
+        // label="Nickname"
         onChange={(event) => {
           setUsername(event.target.value)
-        }} 
+        }}
       />
       <h4>Enter chatroom</h4>
-      <input 
-        type="text" 
+      <input
+        type="text"
         placeholder='Chatroom'
-        label="Enter chatroom" 
+        // label="Enter chatroom"
         onChange={(event) => {
           setUsername(event.target.value)
-        }} 
+        }}
       />
       <button>JOIN</button>
     </div>
