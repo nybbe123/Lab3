c815a460ca8435cebaac20ae9bbddba52611a6be
Jonatan Nyberg <33462941+nybbe123@users.noreply.github.com>
2022-05-04 11:05:57 +0200

    conflicts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -36,27 +36,17 @@ function App() {
   // }
 
   return (
-<<<<<<< HEAD
     <div className={classes['main-container']}>
       <div className={classes['left-container']}>
         <img src={logo} alt="logotype" className={classes.logotype} />
         <div className={classes['login-container']}>
           <h3>Create New Chat</h3>
           <div className={classes['username-container']}>
-=======
-    <div className={classes["main-container"]}>
-      <div className={classes["left-container"]}>
-        <img src={logo} alt="logotype" className={classes.logotype} />
-        <div className={classes["login-container"]}>
-          <h3>Create New Chat</h3>
-          <div className={classes["username-container"]}>
->>>>>>> 78bb925da507fc20385e4af4ef5b69b133878e54
             <label htmlFor="nickname">Nickname</label>
             <input
               type="text"
               name="nickname"
               id="nickname"
-<<<<<<< HEAD
               placeholder='Enter your nickname'
               // label="Nickname"
               onChange={(event) => {
@@ -84,40 +74,6 @@ function App() {
       <div className={classes['right-container']}>
         <h3>Free Online Chat Application</h3>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem molestias nulla doloremque, quo rerum assumenda veritatis quam quasi. Vel maiores ex dolorum dolor unde corporis quos temporibus aspernatur.</p>
-=======
-              placeholder="Enter your nickname"
-              // label="Nickname"
-              onChange={(event) => {
-                setUsername(event.target.value);
-              }}
-            />
-          </div>
-          <div className={classes["room-container"]}>
-            <label htmlFor="room">Room Name</label>
-            <input
-              type="text"
-              name="room"
-              id="room"
-              placeholder="Enter chatroom"
-              // label="Enter chatroom"
-              onChange={(event) => {
-                setUsername(event.target.value);
-              }}
-            />
-          </div>
-          <button className="continue-btn" type="submit">
-            CONTINUE
-          </button>
-        </div>
-      </div>
-      <div className={classes["right-container"]}>
-        <h3>Free Online Chat Application</h3>
-        <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem
-          molestias nulla doloremque, quo rerum assumenda veritatis quam quasi.
-          Vel maiores ex dolorum dolor unde corporis quos temporibus aspernatur.
-        </p>
->>>>>>> 78bb925da507fc20385e4af4ef5b69b133878e54
       </div>
     </div>
   );
