a35f72c0f7fbc90fe8f828146066f2bcb1b41fe1
Jonatan Nyberg <33462941+nybbe123@users.noreply.github.com>
2022-05-05 09:35:16 +0200

    idk

--- server/server.ts
+++ server/server.ts
@@ -51,19 +51,3 @@ io.on("connection", (socket) => {
 server.listen(port, () => {
     console.log(`Server is working on port https://localhost:${port}`)
 });
-
-
-
-
-
-
-
-// socket.emit("connected", "welcome to out chat app!"); //Skickar ut meddelande till specifik socket. Parameter 1. är nyckeln från interface och parameter två är meddelande
-
-// socket.on("message", (message) => { //Lyssnar på meddelanden med nyckeln "chat message".
-//     io.emit("message", message); //Då körs nästa funktion som i sin tur emittar ut chat message. IO kör emitt til alla sockets och inte bara en specifik
-// });
-
-// socket.on("disconnect", () => { //Lyssnar efter nyckeln disconnect som anropas när någon loggar ut
-//     console.log("user disconnected");
-// });
