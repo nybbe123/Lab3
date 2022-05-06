62b15ea7334b3c8b622c98f2b9311e12f0ad09b9
Jonatan Nyberg <33462941+nybbe123@users.noreply.github.com>
2022-05-06 15:12:55 +0200

    FIXING OLD VLUES NEVER USED

--- client/src/LoginPage.tsx
+++ client/src/LoginPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect} from "react";
 import { useNavigate } from "react-router-dom";
 
 import classes from "./LoginPage.module.css";
