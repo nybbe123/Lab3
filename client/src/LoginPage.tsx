04c48ceffb55aacb62d0d7cc6c602d4cbf4c3d26
Emil <emil.hagelin@medieinstitutet.se>
2022-05-06 15:31:10 +0200

    autocomplete off test

--- client/src/LoginPage.tsx
+++ client/src/LoginPage.tsx
@@ -8,85 +8,89 @@ import { useSocket } from "./store/SocketProvider";
 
 function LoginPage() {
   const socketCtx = useSocket() as SocketContextType;
-  const navigate = useNavigate(); 
-  
+  const navigate = useNavigate();
+
   const [name, setUsername] = useState("");
   const [room, setRoomName] = useState("");
 
   function onHandleClick() {
-    if(!name.length) {
-        console.log('Username & roomname required...')
-        return;
+    if (!name.length) {
+      console.log("Username & roomname required...");
+      return;
     }
     socketCtx.socket!.auth = {
       username: name,
-    }
+    };
     socketCtx.socket!.emit("join", room);
-    socketCtx.socket!.connect(); 
+    socketCtx.socket!.connect();
   }
-    
+
   useEffect(() => {
     socketCtx.socket?.on("connected", (name) => {
-      console.log(`Connected User: ${name}`)
+      console.log(`Connected User: ${name}`);
       socketCtx.username = name;
-    })
+    });
   }, [socketCtx]);
-    
+
   useEffect(() => {
-    socketCtx.socket?.on('joined', (room) => {
-      console.log(`Users RoomName: ${room}`)
+    socketCtx.socket?.on("joined", (room) => {
+      console.log(`Users RoomName: ${room}`);
       socketCtx.roomName = room;
-      navigate('/rooms');
-    })
+      navigate("/rooms");
+    });
   }, [navigate, socketCtx.socket]);
 
   useEffect(() => {
     socketCtx.socket?.on("connect_error", (err) => {
-      if(err.message === "Invalid username") {
+      if (err.message === "Invalid username") {
         console.log("Invalid username, please try again.");
       }
-    })
+    });
   }, [socketCtx.socket]);
 
-
   return (
-    <div className={classes['main-container']}>
-        <div className={classes['left-container']}>
+    <div className={classes["main-container"]}>
+      <div className={classes["left-container"]}>
         <img src={logo} alt="logotype" className={classes.logotype} />
-        <div className={classes['login-container']}>
-            <h3>Create New Chat</h3>
-            <div className={classes['username-container']}>
+        <div className={classes["login-container"]}>
+          <h3>Create New Chat</h3>
+          <div className={classes["username-container"]}>
             <label htmlFor="username">Nickname</label>
             <input
-                type="text"
-                name="username"
-                id="username"
-                placeholder='Enter Username'
-                autoComplete="off"
-                onChange={(event) => {
-                setUsername(event.target.value)
-                }}
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Enter Username"
+              autoComplete="off"
+              onChange={(event) => {
+                setUsername(event.target.value);
+              }}
             />
-            </div>
-            <div className={classes['room-container']}>
+          </div>
+          <div className={classes["room-container"]}>
             <label htmlFor="roomname">Room Name</label>
             <input
-                type="text"
-                name="roomname"
-                id="roomname"
-                placeholder='Enter Chatroom Name'
-                onChange={(event) => {
-                setRoomName(event.target.value)
-                }}
+              type="text"
+              name="roomname"
+              id="roomname"
+              autoComplete="off"
+              placeholder="Enter Chatroom Name"
+              onChange={(event) => {
+                setRoomName(event.target.value);
+              }}
             />
-            </div>
-              <button onClick={onHandleClick}>CONTINUE</button>
+          </div>
+          <button onClick={onHandleClick}>CONTINUE</button>
         </div>
-        </div>
-        <div className={classes['right-container']}>
+      </div>
+      <div className={classes["right-container"]}>
         <h3>Free Online Chat Application</h3>
-        <p>Welcome to chatroom, your new favorite chat app, it is completly free and showcases multiple different features such as, direct messaging, creating your own rooms setting a username and much more!</p>
-        </div>
+        <p>
+          Welcome to chatroom, your new favorite chat app, it is completly free
+          and showcases multiple different features such as, direct messaging,
+          creating your own rooms setting a username and much more!
+        </p>
+      </div>
     </div>
   );
 }
